a9606d97da67120c8946b40decb286559038e580
Farid Juliano <thanatos.juliano@gmail.com>
2021-02-08 17:00:46 +0700

    prepare to deploy

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const router = require('./routes/index')
-require('dotenv').config()
 const errorHandler = require('./helper/errorHandler')
 var cors = require('cors')
 
